c84948b0e636b625fef35533721914f6f6dfb5e8
Di Wu (Dee Woo) <diwu0111@hotmail.com>
2025-07-31 14:52:37 -0400

    add support for sigmoid hard

---
+++ src/napl/operation/sigmoid.py
@@ -0,0 +1,36 @@
+import torch
+
+from napl.utils import *
+from napl.base import napl_base
+from napl.operation import add_any
+
+
+class sigmoid_hard(napl_base):
+    """
+    This is a fsu scaled addition (input+1)/2.
+    It works for both unipolar and bipolar spike trains.
+    """
+    def __init__(
+        self, 
+        config={
+            'polarity' : 'bipolar'
+        }, 
+    ):
+        super().__init__(config, ['polarity'], polarity_required=True)
+
+        self.scaled_add = add_any({
+            'polarity': self.polarity, 
+            'scale' : 2,
+            'width' : 3,
+            })
+
+
+    def reset(self, verbose=False):
+        self.timestep_cur = 0
+        super().reset(verbose)
+    
+
+    def forward(self, input: torch.tensor):
+        self.tick()
+        return self.scaled_add(torch.stack([input, torch.ones_like(input)], dim=0), dim=0)
+
